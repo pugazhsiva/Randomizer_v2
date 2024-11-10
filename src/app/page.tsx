6f75f977fa767dc80e1ae7aa40d430e49e1ac69a
pugazhsiva <pugazh.siva1@gmail.com>
2024-11-11 05:25:16 +0530

    fixed linter problems, tailwind experiment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
-'use client'
+"use client";
 import { useState } from "react";
 import { Button } from "@nextui-org/react";
 import videoPlayer from "./player";
 
 export default function Home() {
-  const [vidId,setvidId] = useState("Q0lu5bxIcv8")
-  function handleClick()
-{
-  setvidId("ATYbOiAlyN0")
-}
+  const [vidId, setvidId] = useState("");
+  function handleClick() {
+    setvidId("ATYbOiAlyN0");
+  }
 
   return (
-    <div>
-      {videoPlayer(vidId)}
-      <Button onPress={handleClick}>Click me</Button>
-      
+    <div className="flex-auto">
+      <div className="flex justify-center">
+        <h5 className="text-5xl">Randomizer v2</h5>
+      </div>
+      <div className="flex-1 items-center justify-center">
+        {videoPlayer(vidId)}
+        <Button onPress={handleClick}>Click me</Button>
+      </div>
     </div>
   );
 }
-
-
