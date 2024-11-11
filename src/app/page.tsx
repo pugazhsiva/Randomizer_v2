e9f793a03c58f2bd329625c29a496a802edcf43a
pugazhsiva <pugazh.siva1@gmail.com>
2024-11-12 02:40:09 +0530

    added scrollable videos list with async loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@
 import { useState } from "react";
 import { Button } from "@nextui-org/react";
 import videoPlayer from "./player";
+import VideoList from "./videolist";
 
 export default function Home() {
-  const [vidId, setvidId] = useState("");
+  const [vidId, setvidId] = useState("ATYbOiAlyN0");
   function handleClick() {
     setvidId("ATYbOiAlyN0");
   }
@@ -14,9 +15,12 @@ export default function Home() {
       <div className="flex justify-center">
         <h5 className="text-5xl">Randomizer v2</h5>
       </div>
-      <div className="flex-1 items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         {videoPlayer(vidId)}
-        <Button onPress={handleClick}>Click me</Button>
+        <Button onPress={handleClick}>Play</Button>
+      </div>
+      <div className="flex items-center justify-center">
+        <VideoList />
       </div>
     </div>
   );
